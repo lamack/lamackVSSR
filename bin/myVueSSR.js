531b805d1f168edfe1a55fbae6a1d04f9e8d5ec8
lamack <13120612132@163.com>
2022-02-10 00:16:02 +0800

    feat: 引入外部router和store

--- bin/myVueSSR.js
+++ bin/myVueSSR.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 const { fork } = require('child_process')
-const fs = require('fs')
-const path = require('path')
 const program = require('commander');
-const { getOutPath } = require('../lib/util')
+const path = require('path')
+const { getOutPath, rmDir, checkDir } = require('../lib/util')
+const { routerPipe, storePipe, appVuePipe } = require('../lib/pipe')
 program
     .version('1.0.0')
-    .option('build')  //myvuessr build 启动生产构建//TODO: 参数会被带到webpack-cli 一定要先清掉再传给webpack
+    .option('build')  //myvuessr build 启动生产构建// 参数会被带到webpack-cli 一定要先清掉再传给webpack 或者使用干净的子进程
     .option('start')   //myvuessr start 启动生产服务
     .option('serve')   // myuvessr server 启动开发服务
     .option('-m, --mode [type]', 'set mode')
@@ -17,15 +17,21 @@ program
 let DIST_DIR = getOutPath('dist')
 //生产构建
 if (program.build) {
-    try {
-        fs.accessSync(DIST_DIR, fs.constants.R_OK | fs.constants.W_OK);
-        fs.rmSync(DIST_DIR, { recursive: true })
-    } catch (err) {
-    }
-    //无需设置argv 子进程是新的干净的程序
-    // process.argv.splice(2)
-    const serverChild = fork('./children/serverChild.js')
-    const clientChild = fork('./children/clientChild.js')
+    rmDir(DIST_DIR) //先删除dist
+    rmDir(path.resolve(__dirname, '../lib/.memory')) //先删除暂存文件夹
+    checkDir(path.resolve(__dirname, '../lib/.memory')) //再新建
+    Promise.all([routerPipe(), storePipe(), appVuePipe()]).then(() => {
+
+        //执行打包
+        //无需设置argv 子进程是新的干净的程序
+        // process.argv.splice(2)
+        const serverChild = fork('./children/serverChild.js')
+        const clientChild = fork('./children/clientChild.js')
+    }, error => {
+        console.log(error)
+    })
+
+
 }
 // 启动生产服务
 if (program.start) {
